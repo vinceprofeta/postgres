7c3f707231db96fd83c3589db4190beb33147d2f
Vincent.Profeta <Vincent@impossibleventures.com>
2016-10-13 14:04:10 -0500

    knex

--- dist/db/knex.js
+++ dist/db/knex.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var environment = process.env.NODE_ENV || 'development';
-var config = require('../knexfile')[environment];
+var config = require('../../knexfile')[environment];
 var knex = require('knex')(config);
 
 module.exports = knex;
