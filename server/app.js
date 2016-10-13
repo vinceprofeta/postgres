4fc35d6044ed53355e44169c4dec516377c98c81
Vincent.Profeta <Vincent@impossibleventures.com>
2016-10-13 14:26:10 -0500

    babel

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
+require("babel-core/register");
+require("babel-polyfill");
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var worker = require('./app/contextManagers/workerContext');
 
 var routes = require('./app/routes/v1');
