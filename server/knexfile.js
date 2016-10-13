bfc347ab211f7d2843d83bbbac6158078f2a4ab3
Vincent.Profeta <Vincent@impossibleventures.com>
2016-10-13 11:49:41 -0500

    pass

--- server/knexfile.js
+++ server/knexfile.js
@@ -7,6 +7,8 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL + '?ssl=true'
+        connection: process.env.DATABASE_URL + '?ssl=true',
+        password: process.env.DB_PASS,
+        user: process.env.DB_USER
     }
 };
